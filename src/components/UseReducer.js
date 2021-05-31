47c6346ac23b071d64136a52d9815007bc5738fd
Abdullah Al Maruf <thefrankmaruf@gmail.com>
2021-05-31 18:33:08 +0600

    complex useReducer

--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -1,28 +1,53 @@
-import React, { useReducer } from 'react'
+import React, { useReducer,useState } from 'react'
 
 const UseReducer = () => {
 
 
     const reducer = (state, action) => {
         switch(action.type){
-            case "increment":
-                return state + 1;
-            case "decrement":
-                return state - 1;
-            default:
+            case "add-todo":
+                return {todos:[...state.todos,{text:action.text,completed:false}]};
+            case "toggle-todo":
+                return {
+                    todos: state.todos.map((t,idx) =>
+                    idx === action.idx ? {...t, completed: !t.completed} : t
+                    )
+                };
+                default:
                 return state
         }
     }
-    const [count, dispatch] = useReducer(reducer,0)
+    const [{todos}, dispatch] = useReducer(reducer,{todos:[]})
+    const [text,setText] = useState();
     return (
         <div>
-            count: {count}
-            <button
-            onClick={()=>dispatch({type: 'increment'})}
-            >increment</button>
-            <button
-            onClick={()=>dispatch({type: 'decrement'})}
-            >devrement</button>
+            <form
+            onSubmit={e=>{
+                e.preventDefault()
+                dispatch({type:"add-todo",text})
+                setText("")
+            }}
+            >
+            <input
+            value={text}
+            onChange={(e)=>setText(e.target.value)}
+            />
+            </form>
+            {
+                todos.map((t,idx)=>(
+                    <div 
+                    onClick={()=> dispatch({type:"toggle-todo",idx})}
+                    style={{textDecoration: t.completed ? 'line-through': ""}}
+                    key={t.text}>
+                        {t.text}
+                    </div>
+                ))
+            }
+            <pre>
+                {
+                    JSON.stringify(todos,null,2)
+                }
+            </pre>
         </div>
     )
 }
